405a78cb048ed0509e15405fa532e019467746ff
enmalafeev <malafeev.en@yandex.ru>
2017-12-02 22:43:03 +0300

    on change

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,21 @@
 $("#datepicker").datepicker();
 
+// Сохранение данных input в localstorage
+// var elements = document.querySelectorAll('input');
+
+// function checkValidity() {};
+
+// for (i = 0; i < elements.length; i++) {
+//     (function(element) {
+//         var id = element.getAttribute('id');
+//         element.value = sessionStorage.getItem(id); // обязательно наличие у элементов id
+//         element.oninput = function() {
+//             sessionStorage.setItem(id, element.value);
+//             checkValidity();
+//         };
+//     })(elements[i]);
+// }
+
 $(document).ready(function() {
     function sum(num1, num2) {
         return num1 * num2;
@@ -22,6 +38,8 @@ $(document).ready(function() {
         var result = (num1 * num2) + (num3 * num4);
         return result;
     }
+
+
     var count = document.querySelector(".btn");
 
     var value_day_prev = getValue("value-day-prev");
@@ -29,16 +47,6 @@ $(document).ready(function() {
 
     var value_day_current = getValue("value-day-current");
     var value_night_current = getValue("value-night-current");
-
-    count.addEventListener('click', function() {
-        var day_price = getValue("day-price");
-        var night_price = getValue("night-price");
-
-        var value_month_day = getValue("value-month-day");
-        var value_month_night = getValue("value-month-night");
-
-        $("#result").text(getResult(day_price, value_month_day, night_price, value_month_night));
-    });
 
     $("#value-day-current").on('input', function() {
         var value_day_current = getValue("value-day-current");
@@ -48,7 +56,20 @@ $(document).ready(function() {
     $("#value-night-current").on('input', function() {
         var value_night_current = getValue("value-night-current");
         $("#value-month-night").val(sub(value_night_current, value_night_prev));
+
+        count.addEventListener('click', function() {
+            var day_price = getValue("day-price");
+            var night_price = getValue("night-price");
+
+            var value_month_day = getValue("value-month-day");
+            var value_month_night = getValue("value-month-night");
+
+            $("#result").text(getResult(day_price, value_month_day, night_price, value_month_night));
+        });
+
+
     });
+
 
 
 });
