346f3a8b784412cae2d0a5bdc41ad78c6b346568
Evgeny Malafeev <malafeev.en@yandex.ru>
2017-12-14 16:50:48 +0300

    add favicon icon

--- docs/js/script.js
+++ docs/js/script.js
@@ -40,52 +40,34 @@ $(document).ready(function() {
     }
 
 
-    var count = document.querySelector(".btn");
-
-    var value_day_prev = getValue("value-day-prev");
-    var value_night_prev = getValue("value-night-prev");
-
-    var value_day_current = getValue("value-day-current");
-    var value_night_current = getValue("value-night-current");
+    
 
     $("#value-day-current").on('input', function() {
+        var value_day_prev = getValue("value-day-prev");
         var value_day_current = getValue("value-day-current");
         $("#value-month-day").val(sub(value_day_current, value_day_prev));
     });
 
     $("#value-night-current").on('input', function() {
+        var value_night_prev = getValue("value-night-prev");
         var value_night_current = getValue("value-night-current");
         $("#value-month-night").val(sub(value_night_current, value_night_prev));
-
-        count.addEventListener('click', function() {
-            var day_price = getValue("day-price");
-            var night_price = getValue("night-price");
-
-            var value_month_day = getValue("value-month-day");
-            var value_month_night = getValue("value-month-night");
-
-            $("#result").text(getResult(day_price, value_month_day, night_price, value_month_night));
-        });
-
-
     });
 
+    var count = document.querySelector(".btn");
 
+    count.addEventListener('click', function () {
+        var day_price = getValue("day-price");
+        var night_price = getValue("night-price");
+
+        var value_month_day = getValue("value-month-day");
+        var value_month_night = getValue("value-month-night");
+
+        $("#result").text(getResult(day_price, value_month_day, night_price, value_month_night));
+    });
 
 });
 
 
 
 
-
-
-
-
-
-
-// console.log(count);
-// console.log(sum(3.5, 10));
-// console.log(add(3.5, 10));
-// console.log(getValue("day-price"));
-// console.log(getValue("night-price"));
-// console.log(getResult(2, 2, 3, 3));
