588a6a77ff7d9be0866480a428700acfef683235
enmalafeev <malafeev.en@yandex.ru>
2018-04-23 15:46:02 +0300

    build for table

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,9 @@
 $("#datepicker").datepicker();
 
+var dateFormat = $("#datepicker").datepicker("option", "dateFormat");
+
+$("#datepicker").datepicker("option", "dateFormat", "dd-mm-yy");
+
 // Сохранение данных input в localstorage
 var elements = document.querySelectorAll('input');
 
@@ -16,57 +20,84 @@ for (i = 0; i < elements.length; i++) {
     })(elements[i]);
 }
 
-$(document).ready(function() {
-    function sum(num1, num2) {
-        return num1 * num2;
-    }
+function sum(num1, num2) {
+    return num1 * num2;
+}
 
-    function add(num1, num2) {
-        return num1 + num2;
-    }
+function add(num1, num2) {
+    return num1 + num2;
+}
 
-    function sub(num1, num2) {
-        return num1 - num2;
-    }
+function sub(num1, num2) {
+    return num1 - num2;
+}
 
-    function getValue(id) {
-        var value = document.getElementById(id).value;
-        return value;
-    }
+function getValue(id) {
+    var value = document.getElementById(id).value;
+    return value;
+}
 
-    function getResult(num1, num2, num3, num4) {
-        var result = (num1 * num2) + (num3 * num4);
-        return result;
-    }
+function getResult(num1, num2, num3, num4) {
+    var result = (num1 * num2) + (num3 * num4);
+    return result;
+}
 
+function getValueDayMonth() {
+    var prev = document.getElementById("value-day-prev").value;
+    var cur = document.getElementById("value-day-current").value;
+    var sub = cur - prev;
+    document.getElementById("value-month-day").value = sub;
+}
 
+function getValueNightMonth() {
+    var prev = document.getElementById("value-night-prev").value;
+    var cur = document.getElementById("value-night-current").value;
+    var sub = cur - prev;
+    document.getElementById("value-month-night").value = sub;
+}
+  
+var count = document.querySelector(".btn");
+
+count.addEventListener('click', function () {
+
+    var tbody = document.getElementsByTagName("TBODY")[0];
+    var row = document.createElement("TR");
+    var td1 = document.createElement("TD");
+    var td2 = document.createElement("TD");
+    var td3 = document.createElement("TD");
+    var td4 = document.createElement("TD");
     
+    tbody.appendChild(row);
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
 
-    $("#value-day-current").on('input', function() {
-        var value_day_prev = getValue("value-day-prev");
-        var value_day_current = getValue("value-day-current");
-        $("#value-month-day").val(sub(value_day_current, value_day_prev));
-    });
+    var day_price = getValue("day-price");
+    var night_price = getValue("night-price");
 
-    $("#value-night-current").on('input', function() {
-        var value_night_prev = getValue("value-night-prev");
-        var value_night_current = getValue("value-night-current");
-        $("#value-month-night").val(sub(value_night_current, value_night_prev));
-    });
+    var datePick = document.getElementById("datepicker").value;
+    var value_month_day = getValue("value-month-day");
+    var value_month_night = getValue("value-month-night");
+    var res = document.querySelector("#result");
 
-    var count = document.querySelector(".btn");
+    td1.innerHTML = datePick;
+    td2.innerHTML = value_month_day;
+    td3.innerHTML = value_month_night;
+    td4.innerHTML = getResult(day_price, value_month_day, night_price, value_month_night);
+    res.innerHTML = getResult(day_price, value_month_day, night_price, value_month_night);
 
-    count.addEventListener('click', function () {
-        var day_price = getValue("day-price");
-        var night_price = getValue("night-price");
-
-        var value_month_day = getValue("value-month-day");
-        var value_month_night = getValue("value-month-night");
-
-        $("#result").text(getResult(day_price, value_month_day, night_price, value_month_night));
-    });
-
+    row.appendChild(td1);
+    row.appendChild(td2);
+    row.appendChild(td3);
+    row.appendChild(td4);
 });
+
+function delRowTable() {
+    document.getElementsByTagName("TBODY")[0].deleteRow(-1);
+}
+
+
 
 
 
